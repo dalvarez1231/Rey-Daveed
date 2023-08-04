ac62a582ed7aabe1af6c27f987e6e38c3e8af2d3
dalvarez1231 <136535693+dalvarez1231@users.noreply.github.com>
2023-08-03 19:17:56 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,122 @@
+// JavaScript Document
+function _(query){
+	return document.querySelector(query);
+}
+function _all(query){
+	return document.querySelectorAll(query);
+}
+let songList = [
+	{
+		thumbnail:"sparkspic.jpg",
+		audio:"Sparks.mp3",
+		songname:"Sparks",
+		artistname:"Rey Daveed"
+	},
+	{
+		thumbnail:"butterfly.jpg",
+		audio:"butterfly.mp3",
+		songname:"Butterfly",
+		artistname:"Rey Daveed"
+	},
+	{
+		thumbnail:"rise.jpg",
+		audio:"riseabove.mp3",
+		songname:"Rise Above",
+		artistname:"Rey Daveed"
+	}
+];
+
+let currentSongIndex = 0;
+
+let player = _(".player"),
+	toggleSongList = _(".player .toggle-list");
+
+let main = {
+	audio:_(".player .main audio"),
+	thumbnail:_(".player .main img"),
+	seekbar:_(".player .main input"),
+	songname:_(".player .main .details h2"),
+	artistname:_(".player .main .details p"),
+	prevControl:_(".player .main .controls .prev-control"),
+	playPauseControl:_(".player .main .controls .play-pause-control"),
+	nextControl:_(".player .main .controls .next-control")
+}
+
+toggleSongList.addEventListener("click", function(){
+	toggleSongList.classList.toggle("active");
+	player.classList.toggle("activeSongList");
+});
+
+_(".player .player-list .list").innerHTML = (songList.map(function(song,songIndex){
+	return `
+		<div class="item" songIndex="${songIndex}">
+			<div class="thumbnail">
+				<img src="../newscenic/${song.thumbnail}">
+			</div>
+			<div class="details">
+				<h2>${song.songname}</h2>
+				<p>${song.artistname}</p>
+			</div>
+		</div>
+	`;
+}).join(""));
+
+let songListItems = _all(".player .player-list .list .item");
+for(let i=0;i<songListItems.length;i++){
+	songListItems[i].addEventListener("click",function(){
+		currentSongIndex = parseInt(songListItems[i].getAttribute("songIndex"));
+		loadSong(currentSongIndex);
+		player.classList.remove("activeSongList");
+	});
+}
+
+function loadSong(songIndex){
+	let song = songList[songIndex];
+	main.thumbnail.setAttribute("src","../newscenic/"+song.thumbnail);
+	document.body.style.background = `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.8)), url("../newscenic/${song.thumbnail}") center no-repeat`;
+	document.body.style.backgroundSize = "cover";	
+	main.songname.innerText = song.songname;
+	main.artistname.innerText = song.artistname;
+	main.audio.setAttribute("src","../newscenic/"+song.audio);
+	main.seekbar.setAttribute("value",0);
+	main.seekbar.setAttribute("min",0);
+	main.seekbar.setAttribute("max",0);
+	main.audio.addEventListener("canplay",function(){
+		main.audio.play();
+		if(!main.audio.paused){
+			main.playPauseControl.classList.remove("paused");
+		}
+		main.seekbar.setAttribute("max",parseInt(main.audio.duration));
+		main.audio.onended = function(){
+			main.nextControl.click();
+		}
+	})
+}
+setInterval(function(){
+	main.seekbar.value = parseInt(main.audio.currentTime);
+},1000);
+
+main.prevControl.addEventListener("click",function(){
+	currentSongIndex--;
+	if(currentSongIndex < 0){
+		currentSongIndex = songList.length + currentSongIndex;
+	}
+	loadSong(currentSongIndex);
+});
+main.nextControl.addEventListener("click",function(){
+	currentSongIndex = (currentSongIndex+1) % songList.length;
+	loadSong(currentSongIndex);
+});
+main.playPauseControl.addEventListener("click",function(){
+	if(main.audio.paused){
+		main.playPauseControl.classList.remove("paused");
+		main.audio.play();
+	} else {
+		main.playPauseControl.classList.add("paused");
+		main.audio.pause();
+	}
+});
+main.seekbar.addEventListener("change",function(){
+	main.audio.currentTime = main.seekbar.value;
+});
+loadSong(currentSongIndex);
